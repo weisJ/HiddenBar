7d9932eac4327db655d2e8172c8ec3d1278e6643
UeharaYou <jack1617728914+github@gmail.com>
2023-07-10 14:41:59 +0900

    Stage Commit: CoreGraphic API test.
    IMPORTANT: DO NOT DISTRIBUTE THIS BRANCH!

---
+++ Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Bridge.h
+//  Hidden Bar
+//
+//  Created by 上原葉 on 6/3/23.
+//  Copyright © 2023 Dwarves Foundation. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+
+#endif /* Bridge_h */
